08a8c03cdc03eb5ecd3728eba5c72c8615367288
user1 <user1@user1-PC>
2019-08-20 15:01:07 +0900

    added controller -oh-

---
+++ GitProject/src/com/git/controller/GitController.java
@@ -0,0 +1,10 @@
+package com.git.controller;
+
+public class GitController {
+	public void printGit() {
+		System.out.println("wow! this is git!");
+	}
+	public int calculator(int a, int b) {
+		return a+b;
+	}
+}
