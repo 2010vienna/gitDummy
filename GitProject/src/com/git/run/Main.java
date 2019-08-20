536d4e7852015adb7933ab6c788217495bd76b51
user1 <user1@user1-PC>
2019-08-20 13:49:21 +0900

    oh

--- GitProject/src/com/git/run/Main.java
+++ GitProject/src/com/git/run/Main.java
@@ -3,8 +3,8 @@ package com.git.run;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello this is my first output!");
-		String name ="choi";
+		// TODO Auto-generated method stub
+
 	}
 
 }
